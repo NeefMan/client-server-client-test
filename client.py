6841c97d798bad6d2ea488b20dcc8565ea7f789b
e-chromebook-1 <ethanlahr334@gmail.com>
2025-04-02 09:00:18 -0400

    Initial commit

---
+++ client.py
@@ -0,0 +1,24 @@
+# Import socket module 
+import socket             
+ 
+# Create a socket object 
+s = socket.socket()         
+ 
+# Define the port on which you want to connect 
+port = 5000             
+ 
+# connect to the server on local computer 
+s.connect(('127.0.0.1', port)) 
+
+s.sendall("user1$user2$This is a message".encode())
+
+try:
+    done = False
+    while not done:
+        packet = s.recv(1024).decode('utf-8', 'ignore')  # Ignores invalid characters
+        print(packet)
+        if packet == '':
+            done = True
+            break
+except socket.error as e:
+    print(f"Socket error: {e}")
---
+++ server.py
@@ -0,0 +1,45 @@
+import socket
+import json
+import time
+
+s = socket.socket()
+
+port = 5000
+
+s.bind(('', port))
+
+s.listen(5)
+
+users = {} # [username] = (ip, port, c_socket)
+
+task_queue = [] # timestamp, from_user, to_user, message
+
+while True:
+    c, addr = s.accept()
+    ip, port = addr
+    print(f"connection from {addr}")
+
+    delimeter = "$"
+
+    data = c.recv(1024).decode().split(delimeter) # [username, connect_to_username, message]
+    if len(data):
+        username = data[0]
+        users[username] = (ip, port, c)
+        users["user2"] = (ip, port, c)
+    if len(data) >= 2:
+        connect_to_username = data[1]
+    if len(data) >= 3:
+        message = data[2]
+        task_queue.append((time.time(), username, connect_to_username, message))
+    
+    temp = []
+    task_max_buffer_time = 10
+    for task in task_queue:
+        timestamp, from_user, to_user, message = task
+        if time.time() - timestamp < 10 and (not from_user in users or not to_user in users):
+            temp.append(task) # If the task cannot be completed for the above reasons, retry next time
+            continue
+        unused, unused, c = users[to_user]
+
+        c.sendall(f"{from_user}{delimeter}{message}".encode())
+    task_queue = temp
