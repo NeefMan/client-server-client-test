e5ee0071a04681b12297f934237ef9a5a3fb6f53
Ethan Lahr <ethanlahr334@gmail.com>
2025-04-03 22:01:31 -0400

    fix

--- client.py
+++ client.py
@@ -22,21 +22,9 @@ while True:
         s.connect(('18.218.245.80', port))
         s.sendall(f"{username}{delimeter}vi".encode())
         
-        data = []  # Moved here
-        done = False
-        while not done:
-            try:
-                packet = s.recv(1024).decode('utf-8', 'ignore')
-                if not packet:
-                    done = True
-                    break
-                data.append(packet)
-            except socket.timeout:
-                done = True  # Break cleanly on timeout
+        print(s.recv(1024).decode('utf-8', 'ignore'))
 
         s.close()
-        print(data)
-        print("".join(data))
     elif task == "sm":
         to_user = input("Who would you like to send it to?")
         message = input("What is the message?")
